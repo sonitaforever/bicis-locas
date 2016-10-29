511be7441c29872f9f42c7ff7d80061a4c831566
Vaio <Vaio>
2016-10-28 23:33:11 -0300

    Bicis locas finalizado

--- js/main.js
+++ js/main.js
@@ -4,50 +4,71 @@ function validateForm(){
     var lastName = document.getElementById('lastname').value;
     var email = document.getElementById('input-email').value;
     var password = document.getElementById('input-password').value;
+    var bicis = document.getElementsByTagName('select')[0];
 
     //Todos los campos son obligatorios, excepto los dos últimos.
     //Los campos nombre y apellido sólo deben permitir caracteres de la A-Z
     function validaNombre(){
-        if(name==null || name.length == 0 || /^\s+$/.test(name) ){
+        if(name==null || name.length == 0 || /^\s+$/.test(name)){
             alert ('Nombre no valido'); 
+            return false;
             }else if(!(name[0]==name[0].toUpperCase())){
                 alert ('Nombre de comenzar con mayusculas');
-            }else if ( /^[a-zA-z\s\ñ\Ñ]+$/){
+            return false;
+            }else if (!(/^[a-zA-z\s\ñ\Ñ]+$/)){
                 alert ('Nombre debe tener solo letras');
+            return false;
             }
         }
-    //validaNombre();
+    validaNombre();
 
     //Para los campos nombre y apellido la primera letra debe ser mayúscula
     function validaApellido(){
         if(lastName == null || lastName.length == 0 || /^\s+$/.test(lastName) ){
             alert ('Apellido no valido'); 
+             return false;
             }else if(!(lastName[0]==lastName[0].toUpperCase())){
                 alert ('Apellido debe comenzar con mayusculas');
-            }else if ( /^[a-zA-z\s\ñ\Ñ]+$/){
+             return false;   
+            }else if (!(/^[a-zA-z\s\ñ\Ñ]+$/)){
                 alert ('Apellido debe tener solo letras');
+             return false;
             }
     }
-    //validaApellido();
+    validaApellido();
     
  
     //Validar que el campo email tenga un formato válido. Ej: name@domain.com
     function validaEmail(){
         if(/^[^@\s]+@[^@\.\s]+(\.[^@\.\s]+)+$/){
-            alert ('Email no valido')
-        }
+            alert ('Email no valido');
+            return false;
+        }else if(name==null || name.length == 0 || /^\s+$/.test(name)){
+            alert ('Email no debe estar vacio');
+            return false;
+        } 
     }
-    //validaEmail();
+    validaEmail();
 
     //El campo password debe tener al menos 6 caracteres
     //El campo password no puede ser igual a "password" ó "123456" ó "098754"
     function validaPassword(){
         if(password.length<6){
             alert ('El campo password debe tener al menos 6 caracteres');
-        }else if( password!="password" || password!="123456" || password!="098754"){
+             return false;
+        }else if( password!='password' || password!='123456' || password!='098754'){
             alert('Contraseña no puede ser igual a password, 123456 o 098754');
+             return false;
         }
     }
-     //validaPassword();
+    validaPassword();
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
     
+    function validaBicis(){
+         if(bicis.value==0){
+            alert ('Debe seleccionar una bicicleta');
+            return false;
+         }
+    }
+    validaBicis();
 }
